decea326d0a7b88d50351d51b0121c849f1e09d1
Michael Borsellino <10859312+mborsel1@users.noreply.github.com>
2018-03-04 17:06:24 -0500

    Add files via upload

---
+++ borsellinomichael1/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__, static_url_path='/static')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
